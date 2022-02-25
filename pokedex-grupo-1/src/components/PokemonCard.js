657286bc2090418cc5ae0552c10005b74ac79621
unknown <Veredaadm@vereda.corp>
2022-02-25 09:37:00 -0300

    Feat: Cria componente PokemonCard para começar a estilização

---
+++ pokedex-grupo-1/src/components/PokemonCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function PokemonCard(pokemon) {
+    <section>
+        <h3>{pokemon.name}</h3>
+        <img href={pokemon.sprites.front_default} />
+    </section>
+}
